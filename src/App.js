fecf17412ca9e6a69152aed9fc8f405c2eb7a8e9
andrewske123 <andrews.albuka@hotmail.com>
2021-09-17 00:26:25 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Contador extends React.Component {
+  constructor(){
+
+    super();  
+    this.state = {
+      contador:0,
+    };
+
+  }
+
+  decrementar(){
+    this.setState({
+      contador: this.state.contador -1,
+    
+    });
+  }
+
+  incrementar(){
+    this.setState({
+      contador: this.state.contador +1,
+    });
+
+  }
+
+  render() {
+      return(
+          <div>
+            <center>
+
+              <h1 id='contador'>Contador de Pessoas</h1>
+              <h1 id='result'>{this.state.contador}</h1>
+
+              <div>
+
+                <button onClick={this.incrementar.bind(this)} id='soma'>+</button>
+                  <br></br>
+                <button onClick={this.decrementar.bind(this)} id='sub'>-</button>
+                  
+              </div>
+
+              </center>
+          </div>
+      );
+  }
 }
-
-export default App;
+ 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import reactDom from "react-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from '*.\App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("App")
+
+)
